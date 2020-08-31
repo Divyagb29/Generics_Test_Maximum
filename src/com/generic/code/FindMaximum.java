f4f3b66ea3fb10d583b79644325b501264fa35ca
Divyagb29 <divya2229mgb@gmail.com>
2020-09-01 03:03:38 +0530

    Refactored(Created one method for all data type extending Comparable Interface)

--- src/com/generic/code/FindMaximum.java
+++ src/com/generic/code/FindMaximum.java
@@ -1,37 +1,15 @@
 package com.generic.code;
 
 public class FindMaximum {
-    public Integer findMaxInteger(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
-        Integer max = firstNumber;
-        if (secondNumber.compareTo(max) > 0) {
-            max = secondNumber;
+    public <T extends Comparable<T>> T findMaxInStringIntegerFloat(T firstposition, T secondposition, T thirdposition) {
+        T max = firstposition;
+        if (secondposition.compareTo(max) > 0) {
+            max = secondposition;
         }
-        if (thirdNumber.compareTo(max) > 0) {
-            max = thirdNumber;
-        }
-        return max;
-    }
-    public Float findMaxFloat(Float firstNumber, Float secondNumber, Float thirdNumber) {
-        Float max = firstNumber;
-        if (secondNumber.compareTo(max) > 0) {
-            max = secondNumber;
-        }
-        if (thirdNumber.compareTo(max) > 0) {
-            max= thirdNumber;
+        if (thirdposition.compareTo(max) > 0) {
+            max= thirdposition;
         }
         return max;
     }
-    public String getMaxString(String firstString, String secondString, String thirdString)
-    {
-        if(firstString.length() > secondString.length() && secondString.length() > thirdString.length()) {
-            return firstString;
-        }
-        if (secondString.length() > 0) {
-            return secondString;
-        }
-        else {
-            return thirdString;
-        }
-    }
-
 }
+
