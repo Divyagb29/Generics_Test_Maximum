030f0f737a9cf3c3145308ad047904ad0e2d50d6
Divyagb29 <divya2229mgb@gmail.com>
2020-09-01 01:16:20 +0530

    added future to find max in 3 integers and test cases to all numbers

---
+++ src/com/generic/code/FindMaximum.java
@@ -0,0 +1,14 @@
+package com.generic.code;
+
+public class FindMaximum {
+    public Integer findMaxInteger(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+        Integer max = firstNumber;
+        if (secondNumber.compareTo(max) > 0) {
+            max = secondNumber;
+        }
+        if (thirdNumber.compareTo(max) > 0) {
+            max = thirdNumber;
+        }
+        return max;
+    }
+}
---
+++ src/com/mood_anayser/main/Main.java
@@ -0,0 +1,8 @@
+package com.mood_anayser.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
